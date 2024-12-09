790d5f1b992057fdaec658b0281daa3b59324dd6
lzlobecki <144738447+lzlobecki@users.noreply.github.com>
2024-12-09 12:14:28 -0500

    Add backend for profiles

---
+++ scripts/balance.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const clientEmail = window.localStorage.getItem('ClientEmail');
+  if (clientEmail) {
+    fetch('http://localhost:3000/api/customer-info', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: clientEmail })
+    })
+    .then(response => response.json())
+    .then(customer => {
+      if (customer.error) {
+        console.error(customer.error);
+      } else {
+        // From Square API: https://developer.squareup.com/reference/square/customers-api/retrieve-customer#response__property-customer
+        const customerInfoDiv = document.getElementById('customer-info');
+        customerInfoDiv.innerHTML = `
+          <h4>${customer.given_name} ${customer.family_name}</h4>
+          <p>Email: ${customer.email_address}</p>
+          <p>Balance: ${customer.reference_id}</p>
+        `;
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  } else {
+    console.error('ClientEmail not found in localStorage');
+  }
+});
---
+++ scripts/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api/customer-info', async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' });
+  }
+
+  try {
+    const response = await axios.get('https://connect.squareup.com/v2/customers?count=true&sort_order=DESC&sort_field=CREATED_AT', {
+      headers: {
+        'Square-Version': '2024-11-20',
+        'Authorization': 'Bearer EAAAls_rU9T80oS74IQZGcq_DHqGWefHeyqA56SQiBNzip03qhBXSGjR2AlQQD-9',
+        'Content-Type': 'application/json',
+        'origin': 'http://localhost:3000'
+      }
+    });
+    const data = response.data;
+    if (data.customers && Array.isArray(data.customers)) {
+      const customer = data.customers.find(customer => customer.email_address === email);
+      if (customer) {
+        return res.json(customer);
+      } else {
+        return res.status(404).json({ error: 'Customer not found' });
+      }
+    } else {
+      return res.status(500).json({ error: 'Failed to fetch customer data' });
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return res.status(500).json({ error: 'Failed to fetch customer data' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
